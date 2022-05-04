bc97e89ef992d6ec9ebf01101605480f37784de7
sagitiminsky <sagitiminsky@gmail.com>
2022-05-04 15:41:21 +0300

    first commit

---
+++ home_assignment.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import os
+from datetime import datetime
+
+
+class Placeholder:
+    def __init__(self):
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    """
+    1.
+    1st part of the assignment
+    """
+    # enter your code here - use Placeholder class - you can change the name
+
+    """
+    2.
+    2nd part of the assignment
+    """
+    # enter your code here - use Placeholder class - you can change the name
+
+    """
+    3.
+    3rd part of the assignment
+    """
+    # etner your code here - use Placeholder class - you can change the name
