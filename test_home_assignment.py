aa6fd53c43977defc402ace8fc4affacd3fb0b6c
sagitiminsky <sagitiminsky@gmail.com>
2022-05-04 16:08:03 +0300

    final fix

--- test_home_assignment.py
+++ test_home_assignment.py
@@ -5,8 +5,8 @@ class TestPlaceholder(unittest.TestCase):
     def setUp(self):
         self.place_holder=Placeholder()
 
-    def test_decision_tre_node(self):
-        # do something with self.place_holder
+    def test_something(self):
+        # something to do with self.place_holder ...
         self.assertTrue(True)
 
 if __name__ == '__main__':
